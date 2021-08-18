0f756773f667af9a0d667afe64bd4bd7bd37a2d8
Igor Mokhnatskyy <55652900+gigorko1993@users.noreply.github.com>
2021-08-18 17:16:03 +0300

    change to hooks

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -1,63 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import FeedbackOptions from "./FeedbackOptions";
 import Section from "./Section";
 import Statistics from "./Statistics";
 import Notification from "./Notification";
 
-export class Feedback extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  addFeedback = (e) => {
-    let name = e.target.name;
-    this.setState((prevState) => {
-      let handler = prevState[name];
-      return (prevState[name] = handler + 1);
-    });
-  };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  countPositiveFeedbackPercentage = () => {
-    return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
-  };
-  render() {
-    const { good, neutral, bad } = this.state;
-    let total = this.countTotalFeedback();
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options="good"
-            onFeedbackButtonClick={this.addFeedback}
+export function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = neutral + good + bad;
+  const positiveFeedbackPercentage = Math.floor((good / total) * 100);
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options="good"
+          onFeedbackButtonClick={() => {
+            setGood((p) => p + 1);
+          }}
+        />
+        <FeedbackOptions
+          options="neutral"
+          onFeedbackButtonClick={() => {
+            setNeutral((p) => p + 1);
+          }}
+        />
+        <FeedbackOptions
+          options="bad"
+          onFeedbackButtonClick={() => {
+            setBad((p) => p + 1);
+          }}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percent={positiveFeedbackPercentage}
           />
-          <FeedbackOptions
-            options="neutral"
-            onFeedbackButtonClick={this.addFeedback}
-          />
-          <FeedbackOptions
-            options="bad"
-            onFeedbackButtonClick={this.addFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              percent={this.countPositiveFeedbackPercentage()}
-            />
-          ) : null}
-          {total ? null : <Notification message="No feedback given" />}
-        </Section>
-      </div>
-    );
-  }
+        ) : null}
+        {total ? null : <Notification message="No feedback given" />}
+      </Section>
+    </div>
+  );
 }
 
 export default Feedback;
